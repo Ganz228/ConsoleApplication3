9d53089229ea6a6bb062f2e25342dc82c4f030d2
Ganz228 <viktorsinkov16@gmail.com>
2023-10-23 23:53:36 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main()
+{
+
+	setlocale(LC_ALL, "Russian");
+
+	double x;
+	double y;
+	double a;
+
+	cout << "Введите x\n ";
+	cin >> x;
+
+	cout << "ВВедите y\n ";
+	cin >> y;
+
+	cout << x << endl;
+	cout << y << endl;
+
+	a = (14 * sin(x) + pow(y, 2)) / (0.92 * pow(cos(x), 3));
+
+	cout << "Результат вычеслений =";
+	cout << a << endl;
+
+	system("pause");
+}
+	
+
+
+
